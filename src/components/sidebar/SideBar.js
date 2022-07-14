d701dd1efccded45674c48d232f6d9c17a9d7c19
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-07-14 18:27:05 +0100

    active bar

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { FaHome, FaBars } from "react-icons/fa";
 import { ImBlogger2 } from "react-icons/im";
 import { NavLink } from "react-router-dom";
 import './SideBar.css'
 
 const SideBar = ({ children }) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggle = () => setIsOpen(!isOpen)
   const menuItems = [
     {
       path: "/",
@@ -18,18 +21,18 @@ const SideBar = ({ children }) => {
   ];
   return (
     <div className="sidebar-container">
-      <div className="sidebar">
+      <div className="sidebar" style={{width: isOpen ? "300px" : "50px"}}>
         <div className="sidebar-top">
-            <h1 className="logo">PJMBlog</h1>
-            <div className="bars">
-                <FaBars />
+            <h1 className="logo" style={{display: isOpen ? "block" : "none"}}>PJMBlog</h1>
+            <div className="bars" style={{marginLeft: isOpen ? "50px" : "0px"}}>
+                <FaBars onClick={toggle}/>
             </div>
         </div>
         {
             menuItems.map((item, index) => (
-                <NavLink to={item.path} key={index} className="sidebar-link">
+                <NavLink to={item.path} key={index} className="sidebar-link" activeClassName="active">
                     <div className="side-icon">{item.icon}</div>
-                    <div className="link-text">{item.name}</div>
+                    <div style={{display: isOpen ? "block" : "none"}} className="link-text">{item.name}</div>
                 </NavLink>
             ))
         }
