5a31613f246f2e6a7dee7894da277d3b14a2b679
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-07-16 16:51:09 +0100

    createBlog stlying

--- src/pages/CreateBlog.js
+++ src/pages/CreateBlog.js
@@ -1,9 +1,33 @@
-
+import "./pageStyle/CreateBlog.css";
 
 const CreateBlog = () => {
   return (
-    <div>CreateBlog</div>
-  )
-}
+    <div className="create-section">
+        <div className="header-text">
+          <h2>Create Blog</h2>
+        </div>
+      <form className="create-container">
+        <div className="create-form-control">
+          <label htmlFor="title">
+            <input type="text" placeholder="Title" className="create-input" />
+          </label>
+        </div>
+        <div className="create-form-control">
+          <label htmlFor="content">
+            <input type="text" placeholder="content" className="create-input-content" />
+          </label>
+        </div>
+        <div className="create-form-control">
+          <label htmlFor="Author">
+            <input type="text" placeholder="Author" className="create-input" />
+          </label>
+        </div>
+        <div className="submit-btn">
+          <button type="submit" className="btn">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default CreateBlog
+export default CreateBlog;
